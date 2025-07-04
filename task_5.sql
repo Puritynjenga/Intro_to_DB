1c31a0306f4afcbce60eebfd525b61344c80c3c5
Purity Njenga <puritynjenga895@gmail.com>
2025-07-04 18:11:11 +0300

    modified test5 that stores information in the customers table insid the alx_book_store db

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,5 @@
-INSERT INTO Customers(customer_id,customer_name,email,address)
-VALUES(1,'Cole Baidoo','cbaidoo@sandtech.com','123 Happiness Ave');
+
+--added one row to the Customers table
+-- This script inserts a row into the Customers table of the alx_book_store database.
+INSERT INTO customer(customer_id, customer_name, email, address)
+VALUES(1,'Cole Baidoo','cbaidoo@sandtech.com','123 Happiness Ave.');
