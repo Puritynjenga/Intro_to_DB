6f7664cd6e66ab87542a133de35a499e6340b3b7
Purity Njenga <puritynjenga895@gmail.com>
2025-07-04 18:21:04 +0300

    added more information in the customers table

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,5 @@
 
 --added one row to the Customers table
 -- This script inserts a row into the Customers table of the alx_book_store database.
-INSERT INTO customer(customer_id, customer_name, email, address)
+INSERT INTO Customers(customer_id, customer_name, email, address)
 VALUES(1,'Cole Baidoo','cbaidoo@sandtech.com','123 Happiness Ave.');
