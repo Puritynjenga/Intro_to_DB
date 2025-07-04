7840f9845847cfeddea94f01cfd8f1aef8597e91
Purity Njenga <puritynjenga895@gmail.com>
2025-07-04 17:44:44 +0300

    modified the file to show the tables

--- task_3.sql
+++ task_3.sql
@@ -2,7 +2,8 @@
 -- This script lists all tables in the current database.
 -- The database name will be passed as an argument to the mysql command.
 -- Example: mysql -u username -p alx_book_store < task_3.sql
-
+USE alx_book_store;
+-- List all tables in the alx_book_store database
 SHOW TABLES;
 
 
